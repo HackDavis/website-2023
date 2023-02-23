1fe4c5972be929ab54bac0dedaebe81019d058f6
trishnasharma <trishnasharma@users.noreply.github.com>
2023-02-22 20:52:11 -0800

    navbar mainSection css
    Co-authored-by: dorothyle <dorothyle@users.noreply.github.com>

--- src/components/mainSection.tsx
+++ src/components/mainSection.tsx
@@ -1,37 +1,45 @@
 import React from 'react'
 import styles from '@/styles/mainSection.module.scss'
+import Image from 'next/image'
+import arrowUp from '@/images/arrowUp.svg'
+import heart from '@/images/heart.svg'
+import calendar from '@/images/calendar.svg'
 
 const MainSection = () => {
     return(
-        <div>
+        <div className={styles.mainSection}>
             <div>
                 cow on icecream img
             </div>
-            <div className='tagline'>
-                <p>
+            <div>
 
-                    create for <br /> social good
-                </p>
-            </div>
-            <div className='event'>
-                <p>
+                <div className={styles.tagline}>
+                    
 
-                    HACKDAVIS <br />
-                    location
-                </p>
-            </div>
-            <div className='main-buttons'>
-                <button>
-                    Register Now
-                </button>
-                <button>
-                    Sponsor
-                </button>
-            </div>
-            <div className='starter-pack-link'>
-                <p>
-                    get started with Starter Pack
-                </p>
+                        create for <br /> <div className={styles.socialGood}> social good </div>
+                    
+                </div>
+                <div className={styles.event}>
+                    
+                        <div className={styles.hackdavis}>HACK<b>DAVIS</b> </div>
+                         <br />
+                        <Image src={calendar} alt='' /> May 20, 2023 @ UCenter
+                    
+                </div>
+                <div className={styles.mainButton}>
+                    <button>
+                        Register Now
+                    </button>
+                    <button className={styles.heartButton}>
+                        <Image src={heart} alt='' /> 
+                    </button> Sponsor
+                </div>
+                <div className={styles.starterPack}>
+                    
+                        Get started with our <a href="https://google.com"><b>Starter Pack  </b>  
+                        <Image src={arrowUp} alt='' /> </a>
+                    
+                </div>
             </div>
         </div>
     )
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styles from '@/styles/navbar.module.scss'
 import HDLogo from '../images/HDLogo2.svg'
+import Moon from '../images/moon.svg'
+import Sun from '../images/sun.svg'
 import Image from 'next/image'
 
 const Navbar = () => {
@@ -10,13 +12,17 @@ const Navbar = () => {
                 <Image src={HDLogo} alt=''/>
             </div>
             <div className={styles.navLinks}>
-                <div> Home </div>
-                <div> About Us </div>
-                <div>  
-                    <label className={styles.switch}>
-                        <input type="checkbox"/>
-                        <span className={styles.sliderRound}></span>
-                    </label>
+                <div className={styles.navLinkHome}> Home </div>
+                <div className={styles.navLinkAboutUs}> About Us </div>
+
+                <div className={styles.switchCont}>
+                    <div className={styles.switchButton}>
+                        <input className={styles.checkbox} type="checkbox" id="switch" />
+                        <label className={styles.toggle} htmlFor="switch">
+                            <Image className={styles.moonIcon} src={Moon} alt=''/>
+                            <Image className={styles.sunIcon} src={Sun} alt=''/>
+                        </label>
+                    </div>
                 </div>
             </div>
         </div>
