d7d3fc626a6fcc7f75df77cabc1fe3f244c8ef43
Alec Atienza <59033593+alecF2@users.noreply.github.com>
2023-02-27 12:02:10 -0800

    add SCSS support and optimize fonts (#3)
    * add sass and rearrange scss files
    
    * add font optimization and assign to css variables
    
    * remove unused Inter font
    
    * fully remove unused Inter font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,47 @@
-import '@/styles/globals.css';
+/* eslint-disable react/no-unknown-property */
+import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
+import { DM_Sans, DM_Mono } from '@next/font/google';
+import localFont from '@next/font/local';
+
+const dmSans = DM_Sans({
+  weight: ['400', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+});
+const dmMono = DM_Mono({
+  weight: ['400', '500'],
+  style: ['normal'],
+  subsets: ['latin'],
+});
+const proximaNova = localFont({
+  src: [
+    {
+      path: '../font/ProximaNova-Regular.woff2',
+      weight: '400',
+    },
+    {
+      path: '../font/ProximaNova-Medium.woff2',
+      weight: '500',
+    },
+    {
+      path: '../font/ProximaNova-Bold.woff2',
+      weight: '700',
+    },
+  ],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --proxima-nova: ${proximaNova.style.fontFamily};
+          --dm-sans: ${dmSans.style.fontFamily};
+          --dm-mono: ${dmMono.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
