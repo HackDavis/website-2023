daa60b145b6098e501745f3d11d0466f90ecdb1d
Alec Atienza <alec@null.net>
2023-03-21 12:41:51 -0700

    switch to inline SVGs

---
+++ src/assets/churnerPropeller.tsx
@@ -0,0 +1,21 @@
+const ChurnerPropeller = () => (
+  <svg
+    width="256"
+    height="37"
+    viewBox="0 0 256 37"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect
+      y="10.3586"
+      width="255.939"
+      height="17.7293"
+      rx="8.86463"
+      fill="white"
+    />
+    <circle cx="126.891" cy="18.5" r="18.5" fill="#005271" />
+    <circle cx="126.891" cy="18.5" r="12.5" fill="white" />
+  </svg>
+);
+
+export default ChurnerPropeller;
--- src/components/bus/churner.tsx
+++ src/components/bus/churner.tsx
@@ -1,24 +1,23 @@
-import Image from 'next/image';
 import React from 'react';
 import styles from '@/styles/bus/churner.module.scss';
-import churner from '@/images/churner.svg';
-import churnerGlassReflection from '@/images/churnerGlassReflection.svg';
-import churnerLiquid from '@/images/churnerLiquid.svg';
-import churnerPipeGroup1 from '@/images/churnerPipeGroup1.svg';
-import churnerPipeGroup2 from '@/images/churnerPipeGroup2.svg';
-import churnerPipeGroup3 from '@/images/churnerPipeGroup3.svg';
-import churnerPropeller from '@/images/churnerPropeller.svg';
+import ChurnerBackground from '@/assets/churnerBackground';
+import ChurnerGlassReflection from '@/assets/churnerGlassReflection';
+import ChurnerLiquid from '@/assets/churnerLiquid';
+import ChurnerPipeGroup1 from '@/assets/churnerPipeGroup1';
+import ChurnerPipeGroup2 from '@/assets/churnerPipeGroup2';
+import ChurnerPipeGroup3 from '@/assets/churnerPipeGroup3';
+import ChurnerPropeller from '@/assets/churnerPropeller';
 
 const Churner = () => (
-  <div style={{ minWidth: 296 }}>
+  <div style={{ minWidth: 297 }}>
     <div className={styles.container}>
-      <Image src={churner} alt="something" />
-      <Image src={churnerLiquid} alt="something" />
-      <Image src={churnerGlassReflection} alt="something" />
-      <Image src={churnerPipeGroup1} alt="something" />
-      <Image src={churnerPipeGroup2} alt="something" />
-      <Image src={churnerPipeGroup3} alt="something" />
-      <Image src={churnerPropeller} alt="something" />
+      <ChurnerBackground />
+      <ChurnerLiquid />
+      <ChurnerGlassReflection />
+      <ChurnerPipeGroup1 />
+      <ChurnerPipeGroup2 />
+      <ChurnerPipeGroup3 />
+      <ChurnerPropeller />
     </div>
   </div>
 );
