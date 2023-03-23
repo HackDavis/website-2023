04e56e7b80458f918617faf9354b8dfb3b2b10ed
Alec Atienza <alec@null.net>
2023-03-23 13:22:02 -0700

    reorganize churner assets

---
+++ src/assets/churner/index.ts
@@ -0,0 +1,13 @@
+export { default as ChurnerBackground } from './churnerBackground';
+
+export { default as ChurnerGlassReflection } from './churnerGlassReflection';
+
+export { default as ChurnerLiquid } from './churnerLiquid';
+
+export { default as ChurnerPipeGroup1 } from './churnerPipeGroup1';
+
+export { default as ChurnerPipeGroup2 } from './churnerPipeGroup2';
+
+export { default as ChurnerPipeGroup3 } from './churnerPipeGroup3';
+
+export { default as ChurnerPropeller } from './churnerPropeller';
--- src/components/bus/churner.tsx
+++ src/components/bus/churner.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from '@/styles/bus/churner.module.scss';
-import ChurnerBackground from '@/assets/churnerBackground';
-import ChurnerGlassReflection from '@/assets/churnerGlassReflection';
-import ChurnerLiquid from '@/assets/churnerLiquid';
-import ChurnerPipeGroup1 from '@/assets/churnerPipeGroup1';
-import ChurnerPipeGroup2 from '@/assets/churnerPipeGroup2';
-import ChurnerPipeGroup3 from '@/assets/churnerPipeGroup3';
-import ChurnerPropeller from '@/assets/churnerPropeller';
+import {
+  ChurnerBackground,
+  ChurnerGlassReflection,
+  ChurnerLiquid,
+  ChurnerPipeGroup1,
+  ChurnerPipeGroup2,
+  ChurnerPipeGroup3,
+  ChurnerPropeller,
+} from '@/assets/churner';
 
 const Churner = () => (
   <div className={styles.container}>
