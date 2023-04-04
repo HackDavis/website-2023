2ab21c7ca23876eacea53d2f8e788e296f645d33
dorothyle <46880658+dorothyle@users.noreply.github.com>
2023-04-04 14:05:16 -0700

    fixed toggle navbar button

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,9 +5,16 @@ import moon from "../images/moon.svg";
 import sun from "../images/sun.svg";
 import Image from "next/image";
 
-const Navbar = () => {
-  const [isLight, setLight] = useState(false);
-  const toggleLight = () => {
+// import styles from "@/styles/navbar.module.css";
+// import React, { useState } from "react";
+// import sun from "../public/images/sun.svg";
+// import moon from "../public/images/moon.svg";
+// import HDLogo from "../public/images/HDLogo2.svg"
+// import Image from "next/image";
+
+function Navbar() {
+  const [isLight, setLight] = useState(true);
+  function toggleLight() {
     setLight(!isLight);
   }
   return (
@@ -18,8 +25,10 @@ const Navbar = () => {
       <div className={styles.navLinks}>
         <div className={styles.navLinkHome}> Home </div>
         <div className={styles.navLinkAboutUs}> About Us </div>
-
-        <div className={isLight ? styles.lightPill : styles.darkPill} onClick={toggleLight}>
+        <div
+          className={isLight ? styles.lightPill : styles.darkPill}
+          onClick={toggleLight}
+        >
           <div className={isLight ? styles.darkCircle : styles.lightCircle}>
             <Image
               className={isLight ? styles.moonIcon : styles.sunIcon}
@@ -31,6 +40,37 @@ const Navbar = () => {
       </div>
     </div>
   );
-};
+}
+
+// export { Navbar };
+
+
+// const Navbar = () => {
+//   const [isLight, setLight] = useState(false);
+//   const toggleLight = () => {
+//     setLight(!isLight);
+//   }
+//   return (
+//     <div className={styles.navbarCont}>
+//       <div className={styles.navLogo}>
+//         <Image src={HDLogo} alt="" />
+//       </div>
+//       <div className={styles.navLinks}>
+//         <div className={styles.navLinkHome}> Home </div>
+//         <div className={styles.navLinkAboutUs}> About Us </div>
+
+//         <div className={isLight ? styles.lightPill : styles.darkPill} onClick={toggleLight}>
+//           <div className={isLight ? styles.darkCircle : styles.lightCircle}>
+//             <Image
+//               className={isLight ? styles.moonIcon : styles.sunIcon}
+//               src={isLight ? moon : sun}
+//               alt=""
+//             />
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
 export default Navbar;
