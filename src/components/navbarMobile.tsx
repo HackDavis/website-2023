689546c46307c14d51fd02ce11584ba09ddda022
dorothyle <46880658+dorothyle@users.noreply.github.com>
2023-04-04 17:42:29 -0700

    started mobile navbar implementation

--- src/components/navbarMobile.tsx
+++ src/components/navbarMobile.tsx
@@ -1,54 +1,62 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import styles from "@/styles/navbarMobile.module.scss";
 import Hamburger from "../images/hamburger.svg";
-import MLHBanner from "../images/MLHBadge.svg";
-import LogoIcon from "../images/HDLogo2.svg";
+import Badge from "../images/MLHBadge.svg";
+import HDLogo from "../images/HDLogo2.svg";
 import { Link } from "react-router-dom";
 import Image from "next/image";
 
 const NavbarMobile = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [logo, setLogo] = useState(true);
-
-  const setNavbarLogo = () => {
-    if(window.scrollY > 0) {
-      setLogo(false);
-    } else {
-      setLogo(true);
-    }
-  };
-  useEffect(()=> {
-    window.addEventListener('scroll', setNavbarLogo);
-    return () => {
-      window.removeEventListener('scroll', setNavbarLogo);
-    };
-  }, []);
+  const [open, setopen] = useState(false);
+  const [atHome, setatHome] = useState(true);
+  const [atAbout, setatAbout] = useState(false);
 
   return (
-    <>
-      <a href="https://mlh.io/seasons/2022/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2021-season&utm_content=white" target="_blank"><Image src={MLHBanner} alt = '' className={`${logo ? "" : `${styles.hide}`} ${isOpen ? `${styles.hide}` : ""} ${styles.mlhbanner}`}/></a>
-      <div className={`d-flex justify-content-between align-items-center ${logo ? "" : isOpen ? "" : `${styles.scroll}`} ${styles.container}`}>
-        { logo ?
-          isOpen ? <Link to="/"><Image src={LogoIcon} alt = '' className={styles.logo} /></Link>: <div className={styles.logo}/> 
-          : <Link to="/"><Image src={LogoIcon} alt = '' className={styles.logo} /></Link>
-        }
-        <Image className={styles.hamburger} src={Hamburger} alt = '' onClick={() => setIsOpen(!isOpen)}/>
-      </div>
-        {/* <div className={`${styles.menuNav} ${isOpen ? `${styles.showMenu}` : ""}`}> */}
-        <div className={isOpen ? styles.showMenu + " " + styles.menuNav : styles.hideMenu}>
-        {/* className={isLight ? styles.lightPill : styles.darkPill} */}
+    <div className={styles.navbarCont}>
+      <div className={styles.navbarItems}>
+        {/* badge */}
+        <Image
+          className={styles.badge}
+          id={styles.badgeImage}
+          src={Badge}
+          alt=""
+        />
 
-          <div className={styles.buttons}>
-            <p>Home</p>
-            <p>About Us</p>
-            {/* <p><Link to="/" className={`${window.location.href == `${window.location.origin}/` ? `${styles.bold_navitem}`: `${styles.button}`}`} >Homepage</Link></p>
-            <p><Link to="/about" className={`${window.location.href == `${window.location.origin}/about` ? `${styles.bold_navitem}`: `${styles.button}`}`}>About us</Link></p>
-            <p><a href='https://merch.hackdavis.io/' target="_blank" className={`${styles.button} ${window.location.href == `https://merch.hackdavis.io/` ? `${styles.bold_navitem}` : `${styles.button}`}`}>Merch Store</a></p> */}
-            {/* <a className={styles.button} href="">Dashboard</a> */}
-          </div>
+        {/* hamburger */}
+        <div
+          className={styles.hamburger}
+          onClick={() => {
+            setopen(!open);
+          }}
+        >
+          <Image src={Hamburger} alt="" />
         </div>
-      <div className={isOpen ? styles.removeBg : styles.addBg}/>
-    </>
+      </div>
+
+      <div
+        className={styles.navMenu}
+        id={open ? styles.openMenu : styles.closeMenu}
+      >
+        <ul>
+          <li
+            onClick={() => {
+              setatHome(true);
+              setatAbout(false);
+            }}
+          >
+            <a className={atHome ? styles.bold : styles.normal} href="/">Home</a>
+          </li>
+          <li
+            onClick={() => {
+              setatHome(false);
+              setatAbout(true);
+            }}
+          >
+            <a className={atAbout ? styles.bold : styles.normal} href="/about">About Us</a>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
