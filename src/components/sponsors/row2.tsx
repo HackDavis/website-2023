9a5862f673aa0ee355d85ed0d2c51d14acf98b8e
trishnasharma <trishnasharma@users.noreply.github.com>
2023-04-05 00:40:22 -0700

    eslint fix

--- src/components/sponsors/row2.tsx
+++ src/components/sponsors/row2.tsx
@@ -1,22 +1,23 @@
-import Image from 'next/image'
-import styles from '@/styles/sponsors2.module.scss'
-import yakult from '@/images/sponsors/yakult.png'
-import pocky from '@/images/sponsors/pocky.svg'
+import Image from 'next/image';
+import styles from '@/styles/sponsors2.module.scss';
+import yakult from '@/images/sponsors/yakult.png';
+import pocky from '@/images/sponsors/pocky.svg';
 
-const Row2 = () => {
-    return (
-        <div className={styles.sponsorsRow}>
-            <div className={`${styles.horizontalScroll} ${styles.row2} ${styles.primary}`}>
-                <Image src={yakult} alt="" className={styles.logo} />
-                <Image src={pocky} alt="" className={styles.logo} />
-                
-            </div>
-            <div className={`${styles.horizontalScroll} ${styles.row2} ${styles.secondary}`}>
-                <Image src={yakult} alt="" className={styles.logo} />
-                <Image src={pocky} alt="" className={styles.logo} />
-            </div>
-        </div>
-    )
-}
+const Row2 = () => (
+  <div className={styles.sponsorsRow}>
+    <div
+      className={`${styles.horizontalScroll} ${styles.row2} ${styles.primary}`}
+    >
+      <Image src={yakult} alt="" className={styles.logo} />
+      <Image src={pocky} alt="" className={styles.logo} />
+    </div>
+    <div
+      className={`${styles.horizontalScroll} ${styles.row2} ${styles.secondary}`}
+    >
+      <Image src={yakult} alt="" className={styles.logo} />
+      <Image src={pocky} alt="" className={styles.logo} />
+    </div>
+  </div>
+);
 
 export default Row2;
