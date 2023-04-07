2b15c7222fdb945032415451b5caecbdacf0d85f
Eric Ni <47995084+ecni19@users.noreply.github.com>
2023-04-06 18:49:53 -0700

    Make navbar changes (#20)

--- src/components/navbar/navbarMobile.tsx
+++ src/components/navbar/navbarMobile.tsx
@@ -1,50 +1,84 @@
-import React, { useState } from 'react';
-import styles from '@/styles/navbar/navbarMobile.module.scss';
-import Image from 'next/image';
-import Link from 'next/link';
-import Hamburger from '@/images/hamburger.svg';
-import Badge from '@/images/MLHBadge.svg';
+import React, { useEffect, useState } from "react";
+import styles from "@/styles/navbar/navbarMobile.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+import Hamburger from "@/images/hamburger.svg";
+import Badge from "@/images/MLHBadge.svg";
+import HDLogo from "@/images/HDLogo.svg";
 
 const NavbarMobile = () => {
-  const [open, setopen] = useState(false);
-  const [atHome, setatHome] = useState(true);
-  const [atAbout, setatAbout] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [atHome, setAtHome] = useState(true);
+  const [atAbout, setAtAbout] = useState(false);
+  const [isScroll, setIsScroll] = useState(false);
+  const setNavbarLogo = () => {
+    if (window.scrollY > 0) {
+      setIsScroll(true);
+    } else {
+      setIsScroll(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", setNavbarLogo);
+    return () => {
+      window.removeEventListener("scroll", setNavbarLogo);
+    };
+  }, []);
 
   return (
-    <div className={styles.navbarCont}>
-      <div className={styles.navbarItems}>
+    <div
+      className={`${styles.navbarCont} ${
+        isScroll && !open ? `${styles.showShadow}` : ""
+      }`}
+    >
+      <div
+        className={`${styles.navbarItems} ${
+          isScroll ? `${styles.navbarItemsOnScroll}` : ""
+        }`}
+      >
         {/* badge */}
-        <Image
-          className={styles.badge}
-          id={styles.badgeImage}
-          src={Badge}
-          alt=""
-        />
+        {isScroll || open ? (
+          <a href="/">
+            <Image
+              className={`${isScroll || open ? `${styles.showLogo}` : ""}`}
+              src={HDLogo}
+              alt="HackDavis Logo"
+            />
+          </a>
+        ) : (
+          <a href="https://mlh.io/seasons/2023/events" target="_blank">
+            <Image className={styles.badge} src={Badge} alt="MLH Logo" />
+          </a>
+        )}
 
         {/* hamburger */}
         <button
           className={styles.hamburger}
           onClick={() => {
-            setopen(!open);
+            setOpen(!open);
           }}
           type="button"
         >
-          <Image src={Hamburger} alt="" />
+          <Image src={Hamburger} alt="hamborger" />
         </button>
       </div>
 
       <div
-        className={styles.navMenu}
-        id={open ? styles.openMenu : styles.closeMenu}
+        className={`${styles.navMenu} ${
+          open ? `${styles.navMenuAnimation}` : ""
+        } ${open && isScroll ? styles.navMenuOnScroll : ""}`}
+        id={open ? styles.openMenu : ""}
       >
         <ul>
           <li>
             <Link
-              className={atHome ? styles.bold : styles.normal}
+              className={atHome ? styles.selected : styles.unselected}
               href="/"
               onClick={() => {
-                setatHome(true);
-                setatAbout(false);
+                setAtHome(true);
+                setAtAbout(false);
+                setOpen(false);
               }}
             >
               Home
@@ -52,11 +86,12 @@ const NavbarMobile = () => {
           </li>
           <li>
             <Link
-              className={atAbout ? styles.bold : styles.normal}
+              className={atAbout ? styles.selected : styles.unselected}
               href="/about"
               onClick={() => {
-                setatHome(false);
-                setatAbout(true);
+                setAtHome(false);
+                setAtAbout(true);
+                setOpen(false);
               }}
             >
               About Us
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import Navbar from '@/components/navbar/navbar';
-import MainSection from '@/components/mainSection/mainSection';
-import Faq from '@/components/faq';
-import Footer from '@/components/footer';
-import Sponsors2 from '@/components/sponsors/sponsors2';
-import Bus from '@/components/bus/bus';
+import Navbar from "@/components/navbar/navbar";
+import MainSection from "@/components/mainSection/mainSection";
+import Faq from "@/components/faq";
+import Footer from "@/components/footer";
+import Sponsors2 from "@/components/sponsors/sponsors2";
+import Bus from "@/components/bus/bus";
 
 // Landing page
 export default function Home() {
