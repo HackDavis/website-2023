dce5b4c4584ca1e5519b937507e0e1c24dc07b74
Alec Atienza <alec@null.net>
2023-04-07 01:21:50 -0700

    add SEO stuff
    Co-Authored-By: Eric Ni <47995084+ecni19@users.noreply.github.com>
    Co-Authored-By: Cameron Yee <42549475+Cameron327@users.noreply.github.com>

--- src/components/faq.tsx
+++ src/components/faq.tsx
@@ -40,28 +40,30 @@ const AccordionItem = ({
 const Faq = () => {
   const questions = [
     'What is a hackathon?',
-    "We will be following MLH's Code Of Conduct.",
+    // "We will be following MLH's Code Of Conduct.",
     'Who can attend?',
-    'How does applying as a team work? Why apply as a team?',
+    'Do I need to know how to code?',
+    'How do teams work?',
+    // 'How does applying as a team work? Why apply as a team?',
     'How can I help?',
     'Why social good?',
     'What can I build?',
     "What if I don't have a team or idea?",
     'How much does it cost?',
-    'As an NPO, why should I attend?',
+    // 'As an NPO, why should I attend?',
   ];
 
   const answers = [
-    'answer 1',
-    'answer 2',
-    'answer 3',
-    'answer 4',
-    'answer 5',
-    'answer 6',
-    'answer 7',
-    'answer 8',
-    'answer 9',
-    'answer 10',
+    "A hackathon is where you transform your crazy ideas into real stuff. Hundreds of students from across California form teams around an idea and collaboratively create technical solutions to problems we face in our local communities. These ideas turn into websites, mobile apps, hardware, and more! Come make the most incredible things you can imagine alongside fellow creators. You take care of building and we'll take care of you. We will be following the HackDavis Rules & Policies, and MLH's Code Of Conduct.",
+    "HackDavis is open to all undergraduate and graduate students from all majors and all schools, and graduates who have completed their degrees within the past year. We are also open to high school students above the age of 18. As long as you're prepared to learn, brainstorm, and build cool things, we welcome you to join!",
+    'Nope! HackDavis is proud to be a beginner-focused hackathon, and we welcome hackers from all experience levels and backgrounds - over 40% of our hackers are beginners! HackDavis is the perfect time and place to learn new skills - we host beginner-friendly workshops and our mentors provide guidance to help you build something you can be proud of.',
+    'Teams can have up to four members, and submit a single project together! Want to go solo? Fine by us, but we’d highly recommend joining a team to make new friends and build cool stuff together!',
+    'HackDavis is largely built upon the efforts of our mentors and volunteers, who ensure that our hackers have a great experience at our event! Our mentor and volunteer applications will open in late April, so keep an eye out! If you’re interested in making a tax-deductible contribution to HackDavis, please get in touch at team@hackdavis.io',
+    'We want people to build projects with a meaningful impact and hope to foster a community dedicated to social change. We find that most hackathons often don’t result in projects as practical solutions to specific societal problems and we’re looking to change that.',
+    "You can build whatever your heart desires, whether it's a web app, mobile app, hardware hack, open source tool, or even a simple website! Any social good oriented project is recommended. Our main focus is to bring you a learning experience where you can have fun, ask questions, and experiment with technology.",
+    "Worry not! We also host a series of mixers before HackDavis, so check out our social media for announcements about those as we get closer to the event. We’ll also have a final mixer right after hacking starts during HackDavis, so we'll make sure you're equipped and ready to go! Our non-profit partners, sponsors, and mentors are always here at the event to help you brainstorm and ideate if you don’t have an idea!",
+    'Zero. Zip. Zilch. Nada. HackDavis is free for all admitted participants, so don’t sweat it! We will provide your WiFi, meals, caffeine, swag, and workspace for the entire weekend.',
+    // 'answer 10',
   ];
 
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,17 +5,34 @@ import Footer from '@/components/footer';
 import Sponsors2 from '@/components/sponsors/sponsors2';
 import Bus from '@/components/bus/bus';
 import Clouds from '@/components/mainSection/clouds';
+import Head from 'next/head';
 
 // Landing page
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>HackDavis 2023 | May 2023</title>
+        <meta
+          name="description"
+          content="HackDavis is UC Davis' annual hackathon for students, run by students."
+        ></meta>
+        <meta
+          name="keywords"
+          content="HackDavis, social good, tech, hackathon"
+        ></meta>
+        <meta name="author" content="HackDavis"></meta>
+        <meta
+          httpEquiv="Cache-Control"
+          content="no-cache, no-store, must-revalidate"
+        ></meta>
+      </Head>
       <Navbar />
       <div
         style={{
           position: 'relative',
           backgroundColor: '#023142',
-          overflowX: 'hidden',
+          contain: 'paint',
           paddingTop: 50,
         }}
       >
