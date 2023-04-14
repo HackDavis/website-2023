d8a09f2419e1415af516aeb49e971412824e8d4b
Alec Atienza <alec@null.net>
2023-04-14 12:13:54 -0700

    update clouds import

--- src/components/mainSection/clouds.tsx
+++ src/components/mainSection/clouds.tsx
@@ -1,11 +1,6 @@
-import Image from 'next/image';
-import cloudBg from '@/images/cloudBg.svg';
 import { MainSectionClouds } from '@/assets/mainSection';
 import styles from '@/styles/mainSection/clouds.module.scss';
 
-const Clouds = () => (
-  <Image src={cloudBg} alt="" className={styles.cloud} />
-  // <MainSectionClouds className={styles.cloud} />
-);
+const Clouds = () => <MainSectionClouds className={styles.cloud} />;
 
 export default Clouds;
