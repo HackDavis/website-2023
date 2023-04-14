584c0fcca3a0a5445e8f1340275ede33f5e7d3d2
Alec Atienza <alec@null.net>
2023-04-14 12:02:54 -0700

    make clouds an inline SVG

---
+++ src/assets/mainSection/index.ts
@@ -0,0 +1,1 @@
+export { MainSectionClouds } from './mainSectionClouds';
--- src/components/mainSection/clouds.tsx
+++ src/components/mainSection/clouds.tsx
@@ -1,7 +1,11 @@
 import Image from 'next/image';
 import cloudBg from '@/images/cloudBg.svg';
+import { MainSectionClouds } from '@/assets/mainSection';
 import styles from '@/styles/mainSection/clouds.module.scss';
 
-const Clouds = () => <Image src={cloudBg} alt="" className={styles.cloud} />;
+const Clouds = () => (
+  <Image src={cloudBg} alt="" className={styles.cloud} />
+  // <MainSectionClouds className={styles.cloud} />
+);
 
 export default Clouds;
