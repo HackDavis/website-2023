634ba90e1cec778c47e916334fee205ee7e0ed96
Alec Atienza <alec@null.net>
2023-05-08 14:45:56 -0700

    add light cloud assets

---
+++ src/assets/mainSection/clouds/cloudSmallLight.tsx
@@ -0,0 +1,31 @@
+export const CloudSmallLight = () => (
+  <svg
+    width="252"
+    height="175"
+    viewBox="0 0 252 175"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle
+      cx="78.8218"
+      cy="78.8218"
+      r="78.8218"
+      transform="matrix(-1 0 0 1 252 0)"
+      fill="#005271"
+    />
+    <circle
+      cx="44.8766"
+      cy="44.8766"
+      r="44.8766"
+      transform="matrix(-1 0 0 1 128.879 69.0405)"
+      fill="#005271"
+    />
+    <rect
+      width="243.626"
+      height="60.8197"
+      rx="30.4099"
+      transform="matrix(-1 0 0 1 243.626 113.994)"
+      fill="#005271"
+    />
+  </svg>
+);
--- src/assets/mainSection/clouds/index.ts
+++ src/assets/mainSection/clouds/index.ts
@@ -10,8 +10,12 @@ export { CloudMedium1 } from './cloudMedium1';
 
 export { CloudMedium2 } from './cloudMedium2';
 
+export { CloudMedium2Light } from './cloudMedium2Light';
+
 export { CloudMedium3 } from './cloudMedium3';
 
 export { CloudSmall } from './cloudSmall';
+
+export { CloudSmallLight } from './cloudSmallLight';
 
 export { CloudTiny } from './cloudTiny';
