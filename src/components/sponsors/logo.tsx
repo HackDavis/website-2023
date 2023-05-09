f933ec17e221852a27d4e08d6b2f073879191502
Alec Atienza <alec@null.net>
2023-05-09 16:52:23 -0700

    Update logo.tsx

--- src/components/sponsors/logo.tsx
+++ src/components/sponsors/logo.tsx
@@ -11,9 +11,15 @@ interface LogoOptions {
 const Logo = ({ imgSrc, url, alt }: LogoOptions) => {
   const imgClass = imgSrc.width / imgSrc.height < 1.75 ? styles.tall : '';
 
+  if (!imgSrc.blurDataURL) {
+    // eslint-disable-next-line no-param-reassign
+    imgSrc.blurDataURL =
+      'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8vx8AAsMBwDLzsnQAAAAASUVORK5CYII=';
+  }
+
   return (
     <li className={styles.container}>
-      <a href={url}>
+      <a href={url} target="_blank" rel="noreferrer">
         <Image src={imgSrc} alt={alt} className={imgClass} />
       </a>
     </li>
