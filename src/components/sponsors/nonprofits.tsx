3c0e1891afe3388abfd4c1806d49a176a38deb94
trishnasharma <trishnasharma@users.noreply.github.com>
2023-05-11 14:46:21 -0700

    lint fix

--- src/components/sponsors/nonprofits.tsx
+++ src/components/sponsors/nonprofits.tsx
@@ -1,35 +1,34 @@
 import styles from '@/styles/sponsors/nonprofits.module.scss';
-import Marquee from './marquee';
+// import awhc from '@/images/nonprofits/awhc.png';
+import awhc2 from '@/images/nonprofits/awhc2.svg';
+import reuse from '@/images/nonprofits/reuse.png';
+import treedavis from '@/images/nonprofits/treedavis.png';
 import Logo from './logo';
-import awhc from '@/images/nonprofits/awhc.png'
-import awhc2 from '@/images/nonprofits/awhc2.svg'
-import reuse from '@/images/nonprofits/reuse.png'
-import treedavis from '@/images/nonprofits/treedavis.png'
+import Marquee from './marquee';
 
-const NonProfits = () => {
-    return (
-        <section id="non-profits" className={styles.nonprofitsSection}>
-            <h2 className={styles.support}>support our non-profits.</h2>
-            <div className={styles.nonprofitsContainer}>
-                <Marquee iterations={2} duration={50}>
-                    <Logo imgSrc={reuse} 
-                        url="https://aggiereuse.ucdavis.edu/" 
-                        alt="ASUCD The Aggie Reuse Store Logo" />
-                    <Logo
-                    imgSrc={treedavis}
-                    url="https://www.treedavis.org/"
-                    alt="Tree Davis Logo"
-                    />
-                    <Logo
-                    imgSrc={awhc2}
-                    url="https://www.americanwildhorsecampaign.org/"
-                    alt="American Wild Horse Campaign Logo"
-                    />
-                </Marquee>
-            
-            </div>
-        </section>
-    )
-}
+const NonProfits = () => (
+  <section id="non-profits" className={styles.nonprofitsSection}>
+    <h2 className={styles.support}>support our non-profits.</h2>
+    <div className={styles.nonprofitsContainer}>
+      <Marquee iterations={2} duration={50}>
+        <Logo
+          imgSrc={reuse}
+          url="https://aggiereuse.ucdavis.edu/"
+          alt="ASUCD The Aggie Reuse Store Logo"
+        />
+        <Logo
+          imgSrc={treedavis}
+          url="https://www.treedavis.org/"
+          alt="Tree Davis Logo"
+        />
+        <Logo
+          imgSrc={awhc2}
+          url="https://www.americanwildhorsecampaign.org/"
+          alt="American Wild Horse Campaign Logo"
+        />
+      </Marquee>
+    </div>
+  </section>
+);
 
-export default NonProfits
+export default NonProfits;
--- src/components/sponsors/sponsors.tsx
+++ src/components/sponsors/sponsors.tsx
@@ -2,10 +2,10 @@ import { Heart } from '@/assets/mainSection/misc';
 import chevron from '@/images/sponsors/chevron.png';
 import cs from '@/images/sponsors/cs.png';
 import globalAffairs from '@/images/sponsors/globalaffairs.png';
-import googlecloud from '@/images/sponsors/googlecloud.png'
+import googlecloud from '@/images/sponsors/googlecloud.png';
 import iet from '@/images/sponsors/iet.png';
 import dataLab from '@/images/sponsors/datalab.png';
-import adminit from '@/images/sponsors/adminit.png'
+import adminit from '@/images/sponsors/adminit.png';
 import ssc from '@/images/sponsors/ssc.png';
 import ls from '@/images/sponsors/ls.png';
 import asucd from '@/images/sponsors/asucd.png';
@@ -13,8 +13,8 @@ import pocky from '@/images/sponsors/pocky.svg';
 import yakult from '@/images/sponsors/yakult.png';
 import pepsi from '@/images/sponsors/pepsi.png';
 import cfc from '@/images/sponsors/cfc.png';
-import ultrapress from '@/images/sponsors/ultrapress.png'
-import gt from '@/images/sponsors/gt.png'
+import ultrapress from '@/images/sponsors/ultrapress.png';
+import gt from '@/images/sponsors/gt.png';
 import styles from '@/styles/sponsors/sponsors.module.scss';
 import Marquee from './marquee';
 import Logo from './logo';
@@ -27,9 +27,7 @@ const Sponsors = () => (
     <h2 className={styles.support}>support for social good.</h2>
     <div className={styles.sponsorsContainer}>
       <Marquee iterations={2} duration={50}>
-        <Logo imgSrc={chevron} 
-              url="http://chevron.com/" 
-              alt="Chevron Logo" />
+        <Logo imgSrc={chevron} url="http://chevron.com/" alt="Chevron Logo" />
         <Logo
           imgSrc={cs}
           url="http://cs.ucdavis.edu/"
@@ -53,17 +51,17 @@ const Sponsors = () => (
           alt="UC Davis DataLab Logo"
         />
         <Logo
-            imgSrc={iet}
-            url="http://iet.ucdavis.edu/"
-            alt="UC Davis Information and Educational Technology Logo"
-          />
+          imgSrc={iet}
+          url="http://iet.ucdavis.edu/"
+          alt="UC Davis Information and Educational Technology Logo"
+        />
       </Marquee>
       <Marquee duration={50}>
         <Logo
-            imgSrc={adminit}
-            url="https://adminit.ucdavis.edu/"
-            alt="UC Davis Admin IT Logo"
-          />
+          imgSrc={adminit}
+          url="https://adminit.ucdavis.edu/"
+          alt="UC Davis Admin IT Logo"
+        />
         <Logo
           imgSrc={ssc}
           url="http://startup.ucdavis.edu/"
@@ -86,31 +84,15 @@ const Sponsors = () => (
         />
       </Marquee>
       <Marquee reverse duration={70}>
-        <Logo 
-          imgSrc={pepsi} 
-          url="http://pepsi.com/" 
-          alt="Pepsi Logo" 
+        <Logo imgSrc={pepsi} url="http://pepsi.com/" alt="Pepsi Logo" />
+        <Logo imgSrc={pocky} url="http://pocky.com/" alt="Pocky Logo" />
+        <Logo imgSrc={yakult} url="http://yakultusa.com/" alt="Yakult Logo" />
+        <Logo imgSrc={gt} url="https://gtslivingfoods.com/" alt="GT's Logo" />
+        <Logo
+          imgSrc={ultrapress}
+          url="https://ultrapress.com/"
+          alt="UltraPress Logo"
         />
-        <Logo 
-          imgSrc={pocky} 
-          url="http://pocky.com/" 
-          alt="Pocky Logo" 
-        />
-        <Logo 
-          imgSrc={yakult} 
-          url="http://yakultusa.com/" 
-          alt="Yakult Logo" 
-        />
-        <Logo 
-          imgSrc={gt} 
-          url="https://gtslivingfoods.com/" 
-          alt="GT's Logo" 
-        />
-        <Logo 
-          imgSrc={ultrapress} 
-          url="https://ultrapress.com/" 
-          alt="UltraPress Logo" 
-        />        
       </Marquee>
     </div>
   </section>
