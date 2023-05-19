e6061cb774537330f8e1ac7af24bb83d61628bcd
Alec Atienza <alec@null.net>
2023-05-19 12:44:36 -0700

    fix redirect and schedule

---
+++ src/pages/middleware.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-duplicate-imports */
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+// eslint-disable-next-line consistent-return
+const middleware = (request: NextRequest) => {
+  const url = request.nextUrl.clone();
+
+  if (url.pathname === '/') {
+    url.pathname = '/live';
+
+    return NextResponse.redirect(url);
+  }
+};
+
+export default middleware;
