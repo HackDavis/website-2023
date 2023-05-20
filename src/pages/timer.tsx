abec835a262f35e3b42ce0017723777c4f06b92a
trishnasharma <trishnasharma@users.noreply.github.com>
2023-05-20 07:51:46 -0700

    doe timer

---
+++ src/pages/timer.tsx
@@ -0,0 +1,11 @@
+import Countdown from '@/components/doe/countdown';
+
+const Timer = () => (
+  <>
+    <div style={{ backgroundColor: 'var(--color-teal-3)' }}>
+      <Countdown />
+    </div>
+  </>
+);
+
+export default Timer;
