fece2694c1531d55d042bd5986c4b6d8fa2c7fee
trishnasharma <trishnasharma@users.noreply.github.com>
2023-09-24 00:57:35 -0700

    all team descriptions updated

--- src/components/faq/teamDescriptions.tsx
+++ src/components/faq/teamDescriptions.tsx
@@ -51,7 +51,43 @@ const teamDescriptions = [
   },
   {
     team: 'Operations',
-    description: <>test</>,
+    description: (
+      <>
+        As an Operations Director, you will play a crucial role in ensuring the
+        smooth execution of our initiatives. Your quick decision-making,
+        organization, and responsibility will shine as you tackle challenges
+        head-on. You&apos;ll be a key communicator and leader, working closely
+        with the team and being ready to learn and develop organizational skills
+        as you contribute to the success of one of the biggest hackathons in
+        California.
+        <br />
+        <br />
+        <strong>Qualifications:</strong>
+        <br />
+        <ul>
+          <li>
+            Strong organizational skills with a passion for attention to detail.
+          </li>
+          <li>
+            Quick decision-making abilities, particularly in high-pressure
+            situations.
+          </li>
+          <li>
+            A proactive and responsible approach to tackling challenges with
+            enthusiasm and a willingness to learn and grow.
+          </li>
+          <li>
+            Thrive in a team setting, fostering a spirit of collaboration and
+            mutual support.
+          </li>
+          <li>Excellent communication skills.</li>
+          <li>
+            A passion for learning and developing organizational and leadership
+            skills.
+          </li>
+        </ul>
+      </>
+    ),
   },
   {
     team: 'Marketing',
@@ -135,11 +171,80 @@ const teamDescriptions = [
   },
   {
     team: 'Finance',
-    description: <>test</>,
+    description: (
+      <>
+        As a Finance Director, you will play a vital role in managing our
+        finances, ensuring the sustainability of our initiatives. Your passion
+        and interest in budgeting, financial planning, and fundraising, combined
+        with your outstanding communication skills, will drive our financial
+        success. Your can-do attitude will be an invaluable asset as you
+        contribute to HackDavis&apos; growth.
+        <br />
+        <br />
+        <strong>Qualifications:</strong>
+        <ul>
+          <li>
+            Strong financial acumen with an interest in budgeting and financial
+            planning.
+          </li>
+          <li>
+            Good understanding of cash flow management and financial
+            forecasting.
+          </li>
+          <li>
+            Fundraising experience or a willingness to learn and engage with
+            donors and sponsors effectively.
+          </li>
+          <li>Exceptional communication skills, both written and verbal.</li>
+          <li>
+            A can-do attitude, with the ability to adapt and excel in a
+            fast-paced environment.
+          </li>
+        </ul>
+      </>
+    ),
   },
   {
     team: 'External',
-    description: <>test</>,
+    description: (
+      <>
+        As our External Outreach Coordinator, you will be a driving force behind
+        our external engagement efforts, using your initiative to learn,
+        proactive approach, and personable energy. Familiarity with HackDavis or
+        hackathons is not mandatory, but is a plus, as you will be connecting
+        with clubs on campus, organizations off-campus, industry leaders, our
+        alumni community, and current students, all while confidently and
+        effectively communicating our mission.
+        <br />
+        <br />
+        <strong>Qualifications:</strong>
+        <ul>
+          <li>
+            Spearhead the planning and execution of external events, showcasing
+            your organizational skills and attention to detail.
+          </li>
+          <li>
+            A proactive and personable demeanor, radiating energy and
+            enthusiasm.
+          </li>
+          <li>Strong communication skills, both written and verbal.</li>
+          <li>Confidence in connecting with diverse groups and individuals.</li>
+          <li>
+            Initiative to learn and develop organizational and communication
+            skills.
+          </li>
+          <li>
+            Outreach to clubs on-campus, students and alumni, and off-campus
+            industry leaders; being able to network and foster meaningful
+            connections
+          </li>
+          <li>
+            Familiarity with HackDavis or hackathons is preferred but not
+            mandatory.
+          </li>
+        </ul>
+      </>
+    ),
   },
   {
     team: 'Design',
