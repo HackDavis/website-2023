297047d5c56f984143513853296488912ebe6c35
trishnasharma <trishnasharma@users.noreply.github.com>
2023-09-24 00:18:51 -0700

    tech, design, marketing, spons desc done

--- src/components/faq/team.tsx
+++ src/components/faq/team.tsx
@@ -4,26 +4,33 @@ import teamDescriptions from './teamDescriptions';
 import AccordionItem from './accordionItem';
 
 const Team = () => {
-    const [openedIndex, setOpenedIndex] = useState(-1);
-    return (
-        <section id="faq" style= {{paddingBottom: 0 }}className={styles.faqContainer}>
-          <div className={styles.faqContent}>
-            <h2><center>Team Descriptions</center></h2>
-            <ul className={styles.questionList}>
-              {teamDescriptions.map(({ team, description }, index) => (
-                <AccordionItem
-                  key={team}
-                  itemIndex={index}
-                  openedIndex={openedIndex}
-                  setOpenedIndex={setOpenedIndex}
-                  question={team}
-                  answer={description}
-                />
-              ))}
-            </ul>
-          </div>
-        </section>
-      );
+  const [openedIndex, setOpenedIndex] = useState(-1);
+
+  return (
+    <section
+      id="faq"
+      style={{ paddingBottom: 0 }}
+      className={styles.faqContainer}
+    >
+      <div className={styles.faqContent}>
+        <h2>
+          <center>Team Descriptions</center>
+        </h2>
+        <ul className={styles.questionList}>
+          {teamDescriptions.map(({ team, description }, index) => (
+            <AccordionItem
+              key={team}
+              itemIndex={index}
+              openedIndex={openedIndex}
+              setOpenedIndex={setOpenedIndex}
+              question={team}
+              answer={description}
+            />
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Team;
--- src/components/faq/teamDescriptions.tsx
+++ src/components/faq/teamDescriptions.tsx
@@ -1,67 +1,201 @@
 const teamDescriptions = [
-    {
-        team: 'Technical',
-        description: (
-            <>
-            test
-            
-            </>
-        )
-    },
-    {
-        team: 'Operations',
-        description: (
-            <>
-            test
-            
-            </>
-        )
-    },
-    {
-        team: 'Marketing',
-        description: (
-            <>
-            test
-            
-            </>
-        )
-    },
-    {
-        team: 'Sponsorship',
-        description: (
-            <>
-            test
-            
-            </>
-        )
-    },
-    {
-        team: 'Finance',
-        description: (
-            <>
-            test
-            
-            </>
-        )
-    },
-    {
-        team: 'External',
-        description: (
-            <>
-            test
-            
-            </>
-        )
-    },
-    {
-        team: 'Design',
-        description: (
-            <>
-            test
-            
-            </>
-        )
-    },
-]
+  {
+    team: 'Technical',
+    description: (
+      <>
+        Are you interested in web development with a desire to learn and
+        collaborate with a team of developers? If you have a foundation in
+        HTML/CSS, JavaScript, and/or React.js and are eager to expand your skill
+        set, join the Technical team! As a Technical Director, you will get the
+        chance to collaborate with a team of developers to create and maintain
+        the website and web applications, participate in code review sessions,
+        and offer creative solutions.
+        <br />
+        <br />
+        <strong>Qualifications:</strong>
+        <br />
+        <ul>
+          <li>Basic knowledge of HTML, CSS, and JavaScript.</li>
+          <li>Familiarity with React.js is a plus.</li>
+          <li>
+            Strong desire to learn and grow in the field of web development.
+          </li>
+          <li>Excellent problem-solving skills and attention to detail.</li>
+          <li>
+            Ability to work well in a team and take constructive feedback
+            positively.
+          </li>
+          <li>
+            Passion for technology and a proactive approach to keeping up with
+            industry trends.
+          </li>
+        </ul>
+        <br />
+        <strong>Why Join Us:</strong>
+        <br />
+        <ul>
+          <li>
+            Opportunity to work on diverse and exciting web development
+            projects.
+          </li>
+          <li>Gain hands-on experience and improve your technical skills.</li>
+          <li>Collaborate with a team of motivated developers.</li>
+          <li>Add impressive projects to your resume.</li>
+          <li>Create valuable connections in the tech industry.</li>
+          <li>
+            Personal and professional growth in a student-run organization.
+          </li>
+        </ul>
+      </>
+    ),
+  },
+  {
+    team: 'Operations',
+    description: <>test</>,
+  },
+  {
+    team: 'Marketing',
+    description: (
+      <>
+        As a Marketing Director, you will be right at the heart of our marketing
+        efforts, leveraging your creative thinking, organization,
+        responsibility, and time management skills to help us engage our
+        audience and promote our initiatives effectively. Collaboration with
+        fellow team members and other departments is a key aspect of this role.
+        <br />
+        <br />
+        {/* <strong>Responsibilities</strong>
+            <ul>
+                <li>Creative Thinking: Bring fresh and innovative ideas to our marketing campaigns, creating content that captivates and resonates with our audience.</li>
+                <li>Organization: Assist in maintaining a well-organized marketing calendar, ensuring that campaigns and posts are delivered on time.</li>
+                <li>Time Management: Juggle multiple tasks and deadlines efficiently, keeping marketing initiatives on track.</li>
+                <li>Social Media Management: Manage and curate content for our social media platforms, creating engaging posts and fostering audience interaction.</li>
+                <li>Collaboration: Collaborate with other team members and departments within the organization to align marketing efforts and achieve our collective goals.</li>
+            </ul>
+            <br /> */}
+        <strong>Qualifications:</strong>
+        <ul>
+          <li>
+            Enthusiasm for marketing and a strong desire to learn and grow in
+            the field.
+          </li>
+          <li>Creative mindset with the ability to think strategically.</li>
+          <li>
+            Strong organizational skills and a high level of responsibility.
+          </li>
+          <li>Effective time management abilities.</li>
+          <li>
+            Basic understanding of social media platforms and their dynamics.
+          </li>
+        </ul>
+        <br />
+        <strong>Bonus Skills:</strong>
+        <ul>
+          <li>Prior experience in social media advertising is a plus.</li>
+          <li>
+            Photography skills are a bonus, to generate better quality content
+            for all events including the Day of Event hackathon
+          </li>
+        </ul>
+      </>
+    ),
+  },
+  {
+    team: 'Sponsorship',
+    description: (
+      <>
+        As a Sponsorship Director, you will take the lead in securing vital
+        sponsorships, relying on your confidence and quick thinking to engage
+        potential partners. Your knack for negotiation, proactive approach, and
+        talent for relationship building will be instrumental in fueling our
+        initiatives.
+        <br />
+        <br />
+        <strong>Qualifications:</strong>
+        <ul>
+          <li>
+            Confidence and an ability to think on your feet in negotiations.
+          </li>
+          <li>A natural talent for building and maintaining relationships.</li>
+          <li>
+            Proactive mindset, always on the lookout for new sponsorship
+            opportunities.
+          </li>
+          <li>
+            Strong organizational skills and the ability to manage multiple
+            sponsorship commitments efficiently.
+          </li>
+          <li>
+            A genuine desire to learn and adapt, exploring new outreach and
+            networking strategies.
+          </li>
+        </ul>
+      </>
+    ),
+  },
+  {
+    team: 'Finance',
+    description: <>test</>,
+  },
+  {
+    team: 'External',
+    description: <>test</>,
+  },
+  {
+    team: 'Design',
+    description: (
+      <>
+        Are you a creative, innovative, and proactive designer with a passion
+        for human-centered design? Join our team and help us create
+        brand-consistent, minimal, and aesthetically pleasing designs that
+        captivate and excite our audience. Your creative talent will play a
+        pivotal role in shaping the future of HackDavis. We are looking for
+        talented individuals to join our design team in various roles:
+        <br />
+        <br />
+        <strong>Brand Designer:</strong>
+        <ul>
+          <li>Craft the brand identity for the Hackathon season.</li>
+          <li>Storytelling and experimental design.</li>
+          <li>
+            Proficiency in illustration using Figma and Adobe Illustrator.
+          </li>
+        </ul>
+        <br />
+        <strong>Product Designer:</strong>
+        <ul>
+          <li>
+            Design products that aim to delight and leave an impression,
+            including apparel, stickers, and branded items.
+          </li>
+          <li>Detail-oriented with a focus on product specs.</li>
+          <li>Conduct market research to inform design decisions.</li>
+        </ul>
+        <br />
+        <strong>Content Designer:</strong>
+        <ul>
+          <li>
+            Create eye-catching, compelling content for social media and printed
+            flyers.
+          </li>
+          <li>Deep understanding of the audience.</li>
+          <li>Attention to detail to ensure engaging visuals.</li>
+        </ul>
+        <br />
+        <strong>UI Designer:</strong>
+        <ul>
+          <li>
+            Design beautiful and intuitive web experiences that bring joy to
+            users.
+          </li>
+          <li>Focus on responsive design and intuitive user experiences.</li>
+          <li>
+            Proficiency in wireframing and creating delightful web interfaces.
+          </li>
+        </ul>
+      </>
+    ),
+  },
+];
 
-export default teamDescriptions
+export default teamDescriptions;
