75840a827fe5b0baba4cc6f40987e39d529e8855
Trishna Sharma <70460449+trishnasharma@users.noreply.github.com>
2023-09-24 23:07:26 -0700

    Add typeform link for director applications

--- src/components/mainSection/mainSection.tsx
+++ src/components/mainSection/mainSection.tsx
@@ -65,7 +65,7 @@ const MainSection = () => (
           <div className={styles.buttons}>
             <a
               className={styles.yellowButton}
-              href="https://hackdavis.typeform.com/hacker2023?utm_source=WEBSITE"
+              href="https://form.typeform.com/to/o7xOFqp4?utm_source=WEBSITE"
               target="_blank"
               rel="noreferrer"
             >
